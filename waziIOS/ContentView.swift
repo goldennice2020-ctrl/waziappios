007698526c10dd291e318952f652f12a0ba61688
Eric <1606970967@qq.com>
2026-04-15 23:36:46 +0800

    Initial Commit

---
+++ waziIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  waziIOS
+//
+//  Created by Eric on 2026/4/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ waziIOS/waziIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  waziIOSApp.swift
+//  waziIOS
+//
+//  Created by Eric on 2026/4/15.
+//
+
+import SwiftUI
+
+@main
+struct waziIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
